6191cdafb454d479e6ba0daf458a75a992910a4a
Keeko <787018661@qq.com>
2023-06-16 17:28:57 +0800

    [feat] lc0094 BinaryTreeInorderTraversal

---
+++ problems/src/com/keeko/P0094BinaryTreeInorderTraversal.java
@@ -0,0 +1,39 @@
+package com.keeko;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// 中序遍历 左 根 右 (LDR)
+public class P0094BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inOrder(root, res);
+        return res;
+    }
+
+    public void inOrder(TreeNode root, List<Integer> res) {
+        if (null == root) {
+            return;
+        }
+        inOrder(root.left, res);
+        res.add(root.val);
+        inOrder(root.right, res);
+    }
+}
