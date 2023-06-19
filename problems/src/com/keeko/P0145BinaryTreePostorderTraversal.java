5f44447ba7fcbe2037bbe22ebe97d4c55f878f43
Keeko <787018661@qq.com>
2023-06-19 17:28:51 +0800

    [feat] lc0145 BinaryTreePostorderTraversal

---
+++ problems/src/com/keeko/P0145BinaryTreePostorderTraversal.java
@@ -0,0 +1,38 @@
+package com.keeko;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class P0145BinaryTreePostorderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        postorder(root, res);
+        return res;
+    }
+
+    public void postorder(TreeNode root, List<Integer> res) {
+        if (null == root) {
+            return;
+        }
+        postorder(root.left, res);
+        postorder(root.right, res);
+        res.add(root.val);
+    }
+}
