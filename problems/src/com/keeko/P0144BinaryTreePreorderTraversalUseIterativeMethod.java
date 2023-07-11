50fd7f98f952f5b0d0b7def654712e40a584955a
Keeko <787018661@qq.com>
2023-07-11 17:25:38 +0800

    [feat] lc0144 BinaryTreePreorderTraversal iterative algorithm

---
+++ problems/src/com/keeko/P0144BinaryTreePreorderTraversalUseIterativeMethod.java
@@ -0,0 +1,25 @@
+package com.keeko;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+
+public class P0144BinaryTreePreorderTraversalUseIterativeMethod {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                res.add(root.val);
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop().right;
+        }
+        return res;
+    }
+}
